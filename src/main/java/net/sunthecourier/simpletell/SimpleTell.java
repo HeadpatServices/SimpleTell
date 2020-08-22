75e486b50e15c73cbd77122cd7dd866a243f00fa
Sun <gabehttps@gmail.com>
2020-08-22 00:23:25 -0700

    Initial commit

---
+++ src/main/java/net/sunthecourier/simpletell/SimpleTell.java
@@ -0,0 +1,21 @@
+package net.sunthecourier.simpletell;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleTell extends JavaPlugin {
+    @Getter
+    private static SimpleTell instance;
+
+    @Override
+    public void onEnable() {
+        net.sunthecourier.simpletell.commands.SimpleTell simpleTell = new net.sunthecourier.simpletell.commands.SimpleTell();
+        getCommand("simpletell").setExecutor(simpleTell);
+        getCommand("simpletell").setTabCompleter(simpleTell);
+    }
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+}
---
+++ src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
@@ -0,0 +1,43 @@
+package net.sunthecourier.simpletell.commands;
+
+import net.sunthecourier.simpletell.Utils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class SimpleTell implements TabExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 2) {
+            return false;
+        }
+        String[] newArgs = Utils.reparseArguments(args);
+
+        Player player = getServer().getPlayer(args[0]);
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i < newArgs.length; i++) {
+            str.append(Utils.parseColors(args[i]));
+        }
+        player.sendMessage(str.toString());
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 0) {
+            return null;
+        } else if (args.length == 1) {
+            return getServer().getOnlinePlayers().stream().map(HumanEntity::getName).filter(s -> s.contains(args[0])).collect(Collectors.toList());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}
