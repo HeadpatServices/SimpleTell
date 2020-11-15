ea0fcd939377a020ea42c5358b09f7cbcd9ffb57
Sun <gabehttps@gmail.com>
2020-11-14 20:55:28 -0800

    convert to SpigotExtentions

--- src/main/java/net/sunthecourier/simpletell/SimpleTell.java
+++ src/main/java/net/sunthecourier/simpletell/SimpleTell.java
@@ -4,18 +4,18 @@ import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpleTell extends JavaPlugin {
-    @Getter
-    private static SimpleTell instance;
+	@Getter
+	private static SimpleTell instance;
 
-    @Override
-    public void onEnable() {
-        net.sunthecourier.simpletell.commands.SimpleTell simpleTell = new net.sunthecourier.simpletell.commands.SimpleTell();
-        getCommand("simpletell").setExecutor(simpleTell);
-        getCommand("simpletell").setTabCompleter(simpleTell);
-    }
+	@Override
+	public void onEnable() {
+		net.sunthecourier.simpletell.commands.SimpleTell simpleTell = new net.sunthecourier.simpletell.commands.SimpleTell();
+		getCommand("simpletell").setExecutor(simpleTell);
+		getCommand("simpletell").setTabCompleter(simpleTell);
+	}
 
-    @Override
-    public void onLoad() {
-        instance = this;
-    }
+	@Override
+	public void onLoad() {
+		instance = this;
+	}
 }
--- src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
+++ src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
@@ -1,50 +1,29 @@
 package net.sunthecourier.simpletell.commands;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import net.sunthecourier.simpletell.Utils;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import services.headpat.spigotextensions.brigadier.BrigadierExecutor;
+import services.headpat.spigotextensions.brigadier.argumenttypes.PlayerArgumentType;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import static services.headpat.spigotextensions.brigadier.argumenttypes.PlayerArgumentType.player;
 
-import static org.bukkit.Bukkit.getServer;
-
-public class SimpleTell implements TabExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length < 2) {
-            return false;
-        }
-        String[] newArgs = Utils.reparseArguments(args);
-
-        Player player = getServer().getPlayer(newArgs[0]);
-
-        if (player == null) {
-            sender.sendMessage(ChatColor.RED + "User not found!");
-            return true;
-        }
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i < newArgs.length; i++) {
-            str.append(Utils.parseColors(newArgs[i]));
-            str.append(" ");
-        }
-        player.sendMessage(str.toString());
-        return true;
-    }
-
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (args.length == 0) {
-            return null;
-        } else if (args.length == 1) {
-            return getServer().getOnlinePlayers().stream().map(HumanEntity::getName).filter(s -> s.contains(args[0])).collect(Collectors.toList());
-        } else {
-            return Collections.emptyList();
-        }
-    }
+public class SimpleTell extends BrigadierExecutor {
+	public SimpleTell() {
+		super(LiteralArgumentBuilder.<CommandSender>literal("simpletell")
+				.then(RequiredArgumentBuilder.<CommandSender, Player>argument("player", player())
+						.then(RequiredArgumentBuilder.<CommandSender, String>argument("string", StringArgumentType.greedyString())
+								.executes(context -> {
+									Player player = PlayerArgumentType.getPlayer(context, "player");
+									String str = StringArgumentType.getString(context, "string");
+									player.sendMessage(Utils.covertColorCodes(str));
+									return 1;
+								})
+						)
+				)
+		);
+	}
 }
