08bb94798f6d15550e930edf51fedfbaf4410756
Sun <gabehttps@gmail.com>
2020-12-02 22:08:06 -0800

    bump and update

--- src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
+++ src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
@@ -3,27 +3,28 @@ package net.sunthecourier.simpletell.commands;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import net.sunthecourier.simpletell.Utils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import services.headpat.spigotextensions.brigadier.BrigadierExecutor;
-import services.headpat.spigotextensions.brigadier.argumenttypes.PlayerArgumentType;
+import services.headpat.spigotextensions.utils.ChatUtils;
 
-import static services.headpat.spigotextensions.brigadier.argumenttypes.PlayerArgumentType.player;
+import static services.headpat.spigotextensions.brigadier.arguments.PlayerArgumentType.getPlayer;
+import static services.headpat.spigotextensions.brigadier.arguments.PlayerArgumentType.player;
+
 
 public class SimpleTell extends BrigadierExecutor {
 	public SimpleTell() {
-		super(LiteralArgumentBuilder.<CommandSender>literal("simpletell")
+		super(dispatcher -> dispatcher.register(LiteralArgumentBuilder.<CommandSender>literal("simpletell")
 				.then(RequiredArgumentBuilder.<CommandSender, Player>argument("player", player())
 						.then(RequiredArgumentBuilder.<CommandSender, String>argument("string", StringArgumentType.greedyString())
 								.executes(context -> {
-									Player player = PlayerArgumentType.getPlayer(context, "player");
+									Player player = getPlayer(context, "player");
 									String str = StringArgumentType.getString(context, "string");
-									player.sendMessage(Utils.covertColorCodes(str));
+									player.sendMessage(ChatUtils.covertColorCodes(str));
 									return 1;
 								})
 						)
 				)
-		);
+		));
 	}
 }
