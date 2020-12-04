e4ff92c2781c167fdc6e7f4459df989f139347e9
Sun <gabehttps@gmail.com>
2020-12-03 16:30:00 -0800

    update SimpleTell.java

--- src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
+++ src/main/java/net/sunthecourier/simpletell/commands/SimpleTell.java
@@ -3,11 +3,13 @@ package net.sunthecourier.simpletell.commands;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import services.headpat.spigotextensions.brigadier.BrigadierExecutor;
 import services.headpat.spigotextensions.utils.ChatUtils;
 
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 import static services.headpat.spigotextensions.brigadier.arguments.PlayerArgumentType.getPlayer;
 import static services.headpat.spigotextensions.brigadier.arguments.PlayerArgumentType.player;
 
@@ -16,11 +18,12 @@ public class SimpleTell extends BrigadierExecutor {
 	public SimpleTell() {
 		super(dispatcher -> dispatcher.register(LiteralArgumentBuilder.<CommandSender>literal("simpletell")
 				.then(RequiredArgumentBuilder.<CommandSender, Player>argument("player", player())
-						.then(RequiredArgumentBuilder.<CommandSender, String>argument("string", StringArgumentType.greedyString())
+						.then(RequiredArgumentBuilder.<CommandSender, String>argument("message", StringArgumentType.greedyString())
 								.executes(context -> {
 									Player player = getPlayer(context, "player");
-									String str = StringArgumentType.getString(context, "string");
-									player.sendMessage(ChatUtils.covertColorCodes(str));
+									String message = getString(context, "message");
+									player.sendMessage(ChatUtils.covertColorCodes(message));
+									context.getSource().sendMessage(ChatColor.GREEN + "Message sent!");
 									return 1;
 								})
 						)
